bcd939f438fcf6635dbfa3fccca10d8a1f6e5116
Code-writer130 <nanamarfo860@gmail.com>
2024-09-20 00:20:58 +0100

    Setting up my environment to match design template

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Platform , View, Text, SafeAreaView} from 'react-native';
+
+
+
+export default function HomeScreen() {
+  return (
+    
+      <View style = {styles.container}>
+        <Text>
+          Hello World!
+        </Text>
+      </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+ container:{
+  alignItems:"center",
+  flex:1,
+  justifyContent: "center",
+  
+ },
+  
+});
